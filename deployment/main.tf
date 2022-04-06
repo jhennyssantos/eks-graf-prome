d0c6f1147774a9465a8aa87b67a9fdeb70f367fd
jhenissantos <jhenifer.santos@zoop.com.br>
2022-04-06 14:34:14 -0300

    feat: tfstate s3

--- deployment/main.tf
+++ deployment/main.tf
@@ -8,3 +8,12 @@ provider "helm" {
     config_path = "~/.kube/config"
   }
 }
+
+terraform {
+  backend "s3" {
+    region  = "us-east-1"
+    bucket  = "tfstates"
+    key     = "eks_latency.tfstate"
+    encrypt = true
+  }
+}
